1894fdd97f9eb71217ff229143a7732879615c11
DUCKIsPossiblyGGose <123905487+DUCKIsPossiblyGGose@users.noreply.github.com>
2025-01-12 13:12:31 -0800

    Add files via upload

---
+++ postwar_calculator.py
@@ -0,0 +1,48 @@
+t1_cp = 4
+t2_cp = 6
+t3_cp = 8
+t4_cp = 12
+t5_cp = 15
+t6_cp = 20
+t7_cp = 25
+t8_cp = 33
+t9_cp = 45
+t10_cp = 60
+t11_cp = 80
+t12_cp = 100
+t13_cp = 125
+
+tier = input("input your troop tier (ex t7, t1, t13) ")
+if tier == 't1':
+    cp_per_ant = t1_cp
+if tier == 't2':
+    cp_per_ant = t2_cp
+if tier == 't3':
+    cp_per_ant = t3_cp
+if tier == 't4':
+    cp_per_ant = t4_cp
+if tier == 't5':
+    cp_per_ant = t5_cp
+if tier == 't6':
+    cp_per_ant = t6_cp
+if tier == 't7':
+    cp_per_ant = t7_cp
+if tier == 't8':
+    cp_per_ant = t8_cp
+if tier == 't9':
+    cp_per_ant = t9_cp
+if tier == 't10':
+    cp_per_ant = t10_cp
+if tier == 't11':
+    cp_per_ant = t11_cp
+if tier == 't12':
+    cp_per_ant = t12_cp
+if tier == 't13':
+    cp_per_ant = t13_cp
+postwar_cap = input('alright, and now i need your postwar cap (ex 1387000) ')
+amount_of_ants = int(postwar_cap) / cp_per_ant
+print('you can train', amount_of_ants)
+time_per_ant = input("how long does it take to train one of these ants? (in seconds) ")
+total_time_seconds = int(time_per_ant) * amount_of_ants
+total_time_hours = total_time_seconds / 3600
+print("you need ", total_time_hours, "hours of speeds ups")
